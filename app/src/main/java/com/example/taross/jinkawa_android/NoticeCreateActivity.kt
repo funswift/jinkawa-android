dbd8839946869f386da2f01f6621256a3e5fafa6
Yoshiaki Shinkawa <y_snkw@Y-snkw.local>
2017-11-12 20:31:31 +0900

    [fix]イベント・お知らせ作成の部署を適応、toolbarにタイトルがついてないActivityにタイトル付け

--- app/src/main/java/com/example/taross/jinkawa_android/NoticeCreateActivity.kt
+++ app/src/main/java/com/example/taross/jinkawa_android/NoticeCreateActivity.kt
@@ -2,12 +2,16 @@ package com.example.taross.jinkawa_android
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class NoticeCreateActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notice_create)
+
+        val toolbar = findViewById(R.id.toolbar_notice_create) as Toolbar
+        toolbar.title = getString(R.string.title_notice_create)
 
         val createButton = findViewById(R.id.button_notice_create)
         createButton.setOnClickListener {
