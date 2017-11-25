97767da781f1d07997bee26437cdcf4d1b0bef2f
ysnkw21 <b1015099@fun.ac.jp>
2017-11-26 00:48:50 +0900

    一時チェックアウト

--- app/src/main/java/com/example/taross/jinkawa_android/UserConfig.kt
+++ app/src/main/java/com/example/taross/jinkawa_android/UserConfig.kt
@@ -8,15 +8,26 @@ import org.json.JSONArray
  */
 
 object UserConfig{
+    val CONFIG_FILE_NAME = "com.example.taross.jinkawa_android.config"
+
+    val KEY_IS_PUSHED = "pushed"
+    val KEY_PARTICIPANT_NAME = "participant.name"
+    val KEY_PARTICIPANT_ADDRESS = "participant.address"
+    val KEY_PARTICIPANT_TELL = "participant.tell"
+    val KEY_PARTICIPANT_AGE = "participant.age"
+    val KEY_PARTICIPANT_SEX = "participant.sex"
+
     var is_pushed = true
     val channel = arrayOf("on", "off")
+
     fun sendPushConfigChange(){
         val installation = NCMBInstallation.getCurrentInstallation()
-        installation.channels = if(is_pushed){
-            JSONArray("[on]")
-        } else{
-            JSONArray("[off]")
-        }
+        installation.channels =
+                if(is_pushed){
+                    JSONArray("[on]")
+                } else{
+                    JSONArray("[off]")
+                }
         installation.saveInBackground()
     }
 }
