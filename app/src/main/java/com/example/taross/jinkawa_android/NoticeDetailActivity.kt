8eb975a39b710bd3e1a2a05fa5f2ace8528baca8
taross <taross@Tarossbook.local>
2017-11-29 14:19:01 +0900

    お知らせにも同様の処理を追加

--- app/src/main/java/com/example/taross/jinkawa_android/NoticeDetailActivity.kt
+++ app/src/main/java/com/example/taross/jinkawa_android/NoticeDetailActivity.kt
@@ -51,7 +51,11 @@ class NoticeDetailActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_notice_list, menu)
+        if (LoginManager.isLogin)
+            LoginManager.account?.let {
+                if (it.auth.any{ it == "all" || it == notice.department})
+                    menuInflater.inflate(R.menu.menu_notice_list, menu)
+            }
         return true
     }
 
