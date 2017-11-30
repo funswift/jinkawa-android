627ccfc6f1526537a066c1132acc09dec8e24cdd
ysnkw21 <b1015099@fun.ac.jp>
2017-11-30 19:11:42 +0900

    [add]お知らせ編集にtype項目を追加

--- app/src/main/java/com/example/taross/jinkawa_android/NoticeCreateActivity.kt
+++ app/src/main/java/com/example/taross/jinkawa_android/NoticeCreateActivity.kt
@@ -30,15 +30,15 @@ open class NoticeCreateActivity : AppCompatActivity(), DoneCallback {
 
         val departmentSpinner : Spinner = findViewById(R.id.spinner_department) as Spinner
 
-        var personalAdapter: ArrayAdapter<String> = ArrayAdapter(applicationContext, R.layout.spinner_item)
+        var departmentPersonalAdapter: ArrayAdapter<String> = ArrayAdapter(applicationContext, R.layout.spinner_item)
         LoginManager.account?.let {
-            personalAdapter =
+            departmentPersonalAdapter =
                     if (it.auth.any{it == "all"})
                         ArrayAdapter(applicationContext, R.layout.spinner_item, resources.getStringArray(R.array.array_departments))
                     else
                         ArrayAdapter(applicationContext, R.layout.spinner_item, it.auth)
         }
-        departmentSpinner.adapter = personalAdapter
+        departmentSpinner.adapter = departmentPersonalAdapter
 
         val typeSpinner : Spinner = findViewById(R.id.spinner_type) as Spinner
         val typePersonalAdapter: ArrayAdapter<String> = ArrayAdapter(applicationContext, R.layout.spinner_item, resources.getStringArray(R.array.array_notice_types))
